bcf1a53d8b4f2c169a92fcc1e1239640ac6b06e3
Aku <aku.uusikolli@gmail.com>
2021-12-19 19:22:32 +0200

    auth middleware skeleton

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -1,3 +1,5 @@
+import { checkAuth } from "../services/spotifyService";
+
 const ensureBearer = (req, res, next) => {
   const { X_expires, X_refresh_token } = req.headers;
   if (!X_expires || !X_refresh_token) {
@@ -9,5 +11,20 @@ const ensureBearer = (req, res, next) => {
     console.log("Refreshing token");
   } else {
     next();
+  }
+};
+
+export const checkBearerWithSpotify = async (req, res, next) => {
+  console.log("checkBearerWithSpotify");
+  try {
+    /*  console.log(req.headers); */
+    const token = req.headers.authorization;
+    console.log(token);
+    await checkAuth(token);
+    next();
+  } catch (e) {
+    //TODO: Expired -> do Refresh
+    console.log("checkBearerWithSpotify error");
+    res.sendStatus(403);
   }
 };
