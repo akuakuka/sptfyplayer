8cfe99b414f813ad4177e6afbd9ce32e4b0c5a52
Aku <aku.uusikolli@gmail.com>
2021-12-19 23:52:21 +0200

    hosting changes

--- server/index.ts
+++ server/index.ts
@@ -10,12 +10,14 @@ app.use(cors({ credentials: true, origin: FRONTEND_URL }));
 
 app.use("/api", spotifyRouter);
 app.use("/api/auth/", authRouter);
-if (process.env.NODE_ENV === "dev") {
-  app.listen(PORT, () => {
-    console.info(
-      `Server listening on port ${PORT} MODE = ${process.env.NODE_ENV}`
-    );
-  });
-} else {
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+if (process.env.NODE_ENV !== "dev") {
+  console.log("Serving express static files");
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
 }
+
+app.listen(PORT, () => {
+  console.info(
+    `Server listening on port ${PORT} MODE = ${process.env.NODE_ENV}`
+  );
+});
