e3ff2c6c9a33f4929d1996e83183ec0b440410c7
Aku <aku.uusikolli@gmail.com>
2021-12-26 20:15:38 +0200

    protected route debugging

--- client/src/protectedRoute.tsx
+++ client/src/protectedRoute.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { checkAuth } from "./API";
+import { refreshAccessToken } from "./utils/authUtils";
+import { getExpiryDate } from "./utils/dateUtils";
 
 export const ProtectedRoute: React.FC = () => {
   // let auth = useAuth();
   const location = useLocation();
   const accessToken = localStorage.getItem("accessToken");
+  const expiryDate = localStorage.getItem("expiryDate");
   const [authenticated, setAuthenticated] = useState<boolean>(false);
   const [ready, setReady] = useState<boolean>(false);
   //const navigate = useNavigate();
@@ -12,14 +16,20 @@ export const ProtectedRoute: React.FC = () => {
   useEffect(() => {
     (async () => {
       if (accessToken) {
-        setAuthenticated(true)
-        setReady(true)
+        if(expiryDate && (+expiryDate < getExpiryDate())) {
+          setAuthenticated(true)
+          setReady(true)
+        } else {
+          await refreshAccessToken();
+          setReady(false)
+        }
+
       } else {
         setAuthenticated(false)
         setReady(true)
       }
     })();
-  }, []);
+  }, [accessToken]);
 
   if (ready) {
     if (!authenticated) {
