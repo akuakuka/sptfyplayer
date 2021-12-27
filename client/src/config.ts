a48329bd5c8b6a827368b5f3b1c1e1ef3b9b322b
Aku <aku.uusikolli@gmail.com>
2021-12-27 21:04:38 +0200

    added debugging

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,7 +3,10 @@ const MODE = import.meta.env.MODE
 const BASEURL = MODE === "development" ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/spotify` : `${import.meta.env.VITE_BACKEND_URL_PROD}/api/spotify`
 const REFRESHURL = MODE === "development" ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/auth/refresh` : `${import.meta.env.VITE_BACKEND_URL_PROD}/api/auth/refresh`
 const LOGINURL = MODE === "development" ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/auth/login` : `${import.meta.env.VITE_BACKEND_URL_PROD}/api/auth/login`;
-
+console.log({MODE})
+console.log({BASEURL})
+console.log({REFRESHURL})
+console.log({LOGINURL})
 export {
     BASEURL,
     REFRESHURL,
