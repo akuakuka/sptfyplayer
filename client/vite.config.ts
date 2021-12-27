f3d16f9e1e296da4f330e6cfd20e9c8d6a0c0e74
Aku <aku.uusikolli@gmail.com>
2021-12-27 21:20:14 +0200

    removed pwa for debug

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,26 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 
 export default defineConfig({
-  plugins: [react(),
-  VitePWA({
-    registerType: "autoUpdate",
-    includeAssets: ["/images/**/*"],
-    base: "/",
-
-    manifest: {
-      name: "SPTFYPLAYER",
-      short_name: "SPTFYPLAYER",
-      theme_color: "#333333",
-      display: "standalone",
-      icons: [
-        {
-          src: "/images/icon-512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-  })],
+  plugins: [react()],
   server: { port: 3001 },
   envDir: '../server'
 });
