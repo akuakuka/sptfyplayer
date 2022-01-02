240f9b4251c406691ede3c1f8bee835eacc6a7e0
Aku <aku.uusikolli@gmail.com>
2022-01-02 15:42:27 +0200

    vite plugin for envs

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,26 @@
-// @ts-nocheck
 // TODO : jest import.meta syntax babel?
 import packagejson from "../package.json";
 
 const HOMEPAGE = packagejson.homepage || "https://github.com/";
-
-const MODE = import.meta.env.MODE;
-
+const MODE = process.env.NODE_ENV;
+console.log(process.env.VITE_BACKEND_URL_DEV);
+console.log(process.env.VITE_BACKEND_URL_PROD);
 const BASEURL =
   MODE === "development"
-    ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/spotify`
-    : `${import.meta.env.VITE_BACKEND_URL_PROD}/api/spotify`;
+    ? `${process.env.VITE_BACKEND_URL_DEV}/api/spotify`
+    : `${process.env.VITE_BACKEND_URL_PROD}/api/spotify`;
 
-const REFRESHURL =
-  MODE === "development"
-    ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/auth/refresh`
-    : `${import.meta.env.VITE_BACKEND_URL_PROD}/api/auth/refresh`;
+const REFRESHURL = `${BASEURL}/refresh`;
 
 const LOGINURL =
   MODE === "development"
-    ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/auth/login`
-    : `${import.meta.env.VITE_BACKEND_URL_PROD}/api/auth/login`;
+    ? `${process.env.VITE_BACKEND_URL_DEV}/api/auth/login`
+    : `${process.env.VITE_BACKEND_URL_PROD}/api/auth/login`;
+
+console.log({ BASEURL });
+console.log({ REFRESHURL });
+console.log({ MODE });
+console.log({ LOGINURL });
+console.log({ HOMEPAGE });
 
 export { BASEURL, REFRESHURL, MODE, LOGINURL, HOMEPAGE };
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
-
+import EnvironmentPlugin from "vite-plugin-environment";
+// EnvironmentPlugin so import.meta.key jest problem
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    EnvironmentPlugin([
+      "VITE_BACKEND_URL_DEV",
+      "VITE_BACKEND_URL_PROD",
+      "NODE_ENV",
+    ]),
+  ],
   server: { port: 3001 },
-  envDir: './server',
+  envDir: "./server",
   root: "./src",
   build: {
-    outDir: "./dist"
-  }
+    outDir: "./dist",
+  },
 });
